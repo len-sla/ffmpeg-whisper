a57021e7fc4376cffe06a36c3ff19b32666e860b
len-sla <68053910+len-sla@users.noreply.github.com>
2022-02-05 10:48:35 +0100

    Delete conv2-ok.sh

--- conv2-ok.sh
+++
@@ -1,8 +0,0 @@
-for i in *.mp3;
-
-	do	docker run --rm -d -v $(pwd):$(pwd) -w $(pwd) --name gif-converter jrottenberg/ffmpeg:4.4-ubuntu -i "$i" -acodec pcm_s16le -ar 16000 -ac 1 "$(basename "$i" .mp3)".wav  ;
-	name=`echo "$i" | cut -d'.' -f1`;
-	echo "$name"	;
-
-	sleep 1
-done
